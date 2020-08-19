ea0d78a32b8b8a11c74729a197d08ce549edfaa3
t36campbell <campbell3636@yahoo.com>
2020-08-18 22:45:03 -0400

    foundation of flickr search added

--- src/components/Photos.vue
+++ src/components/Photos.vue
@@ -1,28 +1,47 @@
 <template>
   <v-container fluid>
-      <v-row dense>
+      <v-progress-linear
+        :active="loading"
+        :indeterminate="loading"
+        absolute
+        top
+        color="primary"
+      ></v-progress-linear>
+      <form>
+        <label>
+          Search:
+          <input v-model="tag" type="text">
+        </label>
+        <button type="submit" class="go-button" @click.prevent="search">Search</button>
+      </form>
+      <v-row dense v-if="hasCards">
         <v-col
           v-for="card in cards"
           :key="card.id"
-          :cols="card.flex"
+          :cols="4"
         >
           <v-card>
             <v-img
-              :src="card.src"
+              :src="card.url_n"
               class="white--text align-end"
-              gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
-              height="200px"
+              height="300px"
+              max-height="350px"
             >
-              <v-card-actions>
-                <v-card-title v-text="card.title"></v-card-title>
-                <v-spacer></v-spacer>
-                <v-btn
-                  icon
-                  color="white"
-                >
-                  <v-icon>mdi-heart</v-icon>
-                </v-btn>
-              </v-card-actions>
+              <v-row dense>
+                  <v-col :cols="10">
+                    <v-card-title v-text="card.title"></v-card-title>
+                  </v-col>
+                  <v-col :cols="2">
+                    <v-card-subtitle>
+                      <v-btn
+                        icon
+                        color="white"
+                      >
+                        <v-icon>mdi-heart</v-icon>
+                      </v-btn>
+                    </v-card-subtitle>
+                  </v-col>
+                </v-row>
             </v-img>
           </v-card>
         </v-col>
@@ -30,51 +49,54 @@
   </v-container>
 </template>
 
-<script lang="ts">
+<script>
 import Vue from 'vue';
+import axios from 'axios';
+import flickrAPIKey from '../flickr-config';
 
 export default Vue.extend({
   name: 'Photos',
 
   data: () => ({
-    cards: [
-      {
-        id: 1, title: 'Pre-fab homes', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg', flex: 3,
-      },
-      {
-        id: 2, title: 'Favorite road trips', src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg', flex: 3,
-      },
-      {
-        id: 3, title: 'Best airlines', src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg', flex: 3,
-      },
-      {
-        id: 4, title: 'Pre-fab homes', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg', flex: 3,
-      },
-      {
-        id: 5, title: 'Favorite road trips', src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg', flex: 3,
-      },
-      {
-        id: 6, title: 'Best airlines', src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg', flex: 3,
-      },
-      {
-        id: 7, title: 'Pre-fab homes', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg', flex: 3,
-      },
-      {
-        id: 8, title: 'Favorite road trips', src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg', flex: 3,
-      },
-      {
-        id: 9, title: 'Best airlines', src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg', flex: 3,
-      },
-      {
-        id: 10, title: 'Pre-fab homes', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg', flex: 3,
-      },
-      {
-        id: 11, title: 'Favorite road trips', src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg', flex: 3,
-      },
-      {
-        id: 12, title: 'Best airlines', src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg', flex: 3,
-      },
-    ],
+    loading: false,
+    hasCards: false,
+    tag: '',
+    cards: [],
   }),
+  watch: {
+    loading(val) {
+      if (!val) return;
+      // eslint-disable-next-line no-return-assign
+      setTimeout(() => (this.loading = false), this.timeout);
+    },
+  },
+  methods: {
+    search() {
+      this.loading = true;
+      this.getImages()
+        .then((response) => {
+          this.cards = response.data.photos.photo;
+          this.hasCards = true;
+          this.loading = false;
+        });
+    },
+    getImages() {
+      return axios({
+        method: 'get',
+        url: 'https://api.flickr.com/services/rest',
+        params: {
+          method: 'flickr.photos.search',
+          api_key: flickrAPIKey,
+          tags: this.tag,
+          sort: 'relevance',
+          extras: 'url_n',
+          page: 1,
+          format: 'json',
+          nojsoncallback: 1,
+          per_page: 30,
+        },
+      });
+    },
+  },
 });
 </script>
---
+++ src/flickr-config.js
@@ -0,0 +1,2 @@
+const flickrAPIKey = '1d475ea22e08472b5c87b9206be59e9b';
+export default flickrAPIKey;
