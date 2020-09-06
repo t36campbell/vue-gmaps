4db809c659da6314c08f16e1e5c2221e05258905
t36campbell <campbell3636@yahoo.com>
2020-09-06 11:17:26 -0400

    added firebase to enable analytics

--- src/main.js
+++ src/main.js
@@ -1,11 +1,25 @@
 import Vue from 'vue';
 import * as VueGoogleMaps from 'vue2-google-maps';
+import * as firebase from 'firebase/app';
+import 'firebase/analytics';
 import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
 import vuetify from './plugins/vuetify';
 import vgmAPIKey from './vgm-config';
 
+const firebaseConfig = {
+  apiKey: 'AIzaSyCkClSsMigJ-0Jn0Ny3LwNWmQRtiHOZyLY',
+  authDomain: 'vue-gallery-fe4bb.firebaseapp.com',
+  databaseURL: 'https://vue-gallery-fe4bb.firebaseio.com',
+  projectId: 'vue-gallery-fe4bb',
+  storageBucket: 'vue-gallery-fe4bb.appspot.com',
+  messagingSenderId: '361816645624',
+  appId: '1:361816645624:web:af6395c5543f126716c00d',
+  measurementId: 'G-WK1445X3VE',
+};
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 Vue.config.productionTip = false;
 Vue.use(VueGoogleMaps, {
   load: {
